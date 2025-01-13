1638b2a9adb99875635728978a04bf903669f342
OTMANE <davenamir3@gmail.com>
2025-01-13 19:26:20 +0100

    first puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create first file
 
 file { '/tmp/school':
-  ensure => 'file',
+  ensure  => 'file',
   mode    => '0744',
-  owner => 'www-data'
-  group => 'www-data'
+  owner   => 'www-data'
+  group   => 'www-data'
   content => 'I love Puppet',
 }
