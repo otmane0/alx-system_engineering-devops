804b5780b16e7a4e5a8998d9e95386a64b6201d5
OTMANE <davenamir3@gmail.com>
2025-02-18 15:21:31 +0100

    test

--- 100-puppet_ssh_config.pp
+++ 100-puppet_ssh_config.pp
@@ -1,9 +1,33 @@
 #!/usr/bin/env bash
+# Ensure the ~/.ssh directory exists
+file { '/home/vagrant/.ssh':
+  ensure => directory,
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0700',
+}
 
-file { '/home/ubuntu/.ssh/config':
-  ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+# Ensure the ~/.ssh/config file exists
+file { '/home/vagrant/.ssh/config':
+  ensure  => file,
+  owner   => 'vagrant',
+  group   => 'vagrant',
   mode    => '0600',
-  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+  replace => true,
+}
+
+# Add the IdentityFile line to the SSH config
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/home/vagrant/.ssh/config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^    IdentityFile',
+}
+
+# Add the PasswordAuthentication line to the SSH config
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/home/vagrant/.ssh/config',
+  line   => '    PasswordAuthentication no',
+  match  => '^    PasswordAuthentication',
 }
