20002d8de112751dd90ebdb1b14faea5cc746c33
OTMANE <davenamir3@gmail.com>
2025-02-18 15:17:19 +0100

    puppet config

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",}
+  
